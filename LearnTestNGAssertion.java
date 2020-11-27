b5b77ffae4375627c84d5b17c55c7abbe71fd823
Anandtestauto <66576330+Anandtestauto@users.noreply.github.com>
2020-11-27 05:40:55 +0000

    Add files via upload

---
+++ LearnTestNGAssertion.java
@@ -0,0 +1,60 @@
+package testng.day3;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnTestNGAssertion { 
+	
+	
+	
+	@Test
+	public void createLead() {
+		
+		
+		//System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		
+		SoftAssert sa=new SoftAssert();
+		
+		
+		
+		boolean enabled = driver.findElementByClassName("decorativeSubmit").isEnabled();
+		
+		
+		//Assert.assertTrue(enabled);
+		
+		//Assert.assertFalse(enabled);
+		
+		sa.assertFalse(enabled);
+		
+		
+		String title = driver.getTitle();
+		
+		System.out.println(title);
+		
+		//Assert.assertEquals(title, "Leaftaps - TestLeaf Automation Platform");
+		
+		sa.assertEquals(title, "Leaftaps - TestLeaf Automation Platform");
+		
+		
+		sa.assertAll();
+		
+		
+		
+		
+	}
+
+}
